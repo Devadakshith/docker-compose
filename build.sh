6f73177bf7241e3e40ec03eb202af83d031802b6
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 01:28:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-echo 'building docker images'
+VERSION=$(date +%Y-%m-%d)
+
+echo '****building docker images****'
  
-sudo docker-compose -f docker-compose.yml build
 wait
+sudo docker-compose -f docker-compose.yml build
  
- sudo curl -X GET http://localhost:5000/v2/_catalog
+sudo curl -X GET http://localhost:5000/v2/_catalog
  
- echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
+echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
+  
+  for pat_repo_image in $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}'); do
+    echo "$pat_repo_image"
+	    pat_dev_image=$(echo "$pat_repo_image" | sed "s/$VERSION/latest/")
+	echo "$pat_dev_image"
+    # push images to Docker Registry
+    #docker tag "$pat_repo_image" "$pat_dev_image"
+
+  done
