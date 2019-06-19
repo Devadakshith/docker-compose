308c79546fd1a4111761f80efd2b5dc64136069b
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 01:47:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION=$(date +%Y-%m-%d)
+REPOSITORY="localhost:5000"
 
 echo '****building docker images****'
  
@@ -12,11 +13,9 @@ echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
   
   for pat_repo_image in $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}'); do
     echo "FROM FIRST ECHO $pat_repo_image"
-	echo "Version =====> $VERSION"
-	    pat_dev_image=$(echo "$pat_repo_image" | sed "s/$VERSION/latest/")
-	    echo " from pat dev image ======= $pat_dev_image"
-    # push images to Docker Registry
-    docker tag "$pat_repo_image" "$pat_dev_image"
+    echo "REPO=====>  $REPOSITORY"
+
+    docker tag "$pat_repo_image" "$REPOSITORY/$pat_dev_image"
 	
 	echo $(docker images | awk '{printf "%s:%s\n",$1,$2}')
 
