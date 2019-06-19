af42c8e1036e9c7c481d899518b76c49ba5e30f7
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 02:39:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,8 +19,9 @@ echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
 	#pat_dev_image=$(echo "$pat_repo_image" | sed "s/latest/$REPOSITORY/")
 	
 	#pat_dev_image=$(echo "$pat_repo_image" | awk '{pat_image_repo=$REPOSITORY$pat_dev_image print "$pat_image_repo"}')
-	echo "END ==> $pat_dev_image"
+	
 	REPO_IMAGE=$REPOSITORY$pat_dev_image
+	echo "END ==> $REPO_IMAGE"
 	
 	echo $(docker images | awk '{printf "%s:%s\n",$1,$2}')
 
