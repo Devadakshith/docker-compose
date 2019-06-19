094809199a1e7f09b95d31c16a3264a00121422d
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 00:58:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ sudo docker-compose -f docker-compose.yml build
 wait
  
  sudo curl -X GET http://localhost:5000/v2/_catalog
+ 
+ echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
