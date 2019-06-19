920fa98d8ca87bc8ab7c7c6cb2a3ed12ff7d85b1
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 00:39:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'building docker images'
  
-docker-compose -f docker-compose.yml build
+sudo docker-compose -f docker-compose.yml build
  
- curl -X GET http://localhost:5000/v2/_catalog
+ sudo curl -X GET http://localhost:5000/v2/_catalog
