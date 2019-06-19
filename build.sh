1a212b443c0da7005df4e17c71473480499ffd48
Devadakshith <Devadakshith@users.noreply.github.com>
2019-06-20 01:37:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ sudo curl -X GET http://localhost:5000/v2/_catalog
 echo  $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}')
   
   for pat_repo_image in $(docker images | grep -i tomcat | awk '{printf "%s:%s\n",$1,$2}'); do
-    echo "$pat_repo_image"
+    echo "FROM FIRST ECHO $pat_repo_image"
+	echo "Version =====> $VERSION"
 	    pat_dev_image=$(echo "$pat_repo_image" | sed "s/$VERSION/latest/")
 	    echo " from pat dev image ======= $pat_dev_image"
     # push images to Docker Registry
